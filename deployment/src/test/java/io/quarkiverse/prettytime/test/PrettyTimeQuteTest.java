eaaa0b0222e1f5e99bb36cac1fa43c1dca86c572
Martin Kouba <mkouba@redhat.com>
2021-03-11 09:47:22 +0100

    Add @Named to the PrettyTimeProducer.produce() producer method
    - resolves #4

---
+++ deployment/src/test/java/io/quarkiverse/prettytime/test/PrettyTimeQuteTest.java
@@ -0,0 +1,40 @@
+package io.quarkiverse.prettytime.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Date;
+import java.util.Locale;
+
+import javax.inject.Inject;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.ocpsoft.prettytime.PrettyTime;
+
+import io.quarkus.qute.Template;
+import io.quarkus.test.QuarkusUnitTest;
+
+public class PrettyTimeQuteTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addAsResource(new StringAsset("{inject:prettyTime.format(time)}"), "templates/pretty.txt"));
+
+    @Inject
+    Template pretty;
+
+    @Inject
+    PrettyTime prettyTime;
+
+    @Test
+    public void testNamed() {
+        // Set the reference and locale
+        prettyTime.setReference(new Date(6000));
+        prettyTime.setLocale(Locale.ENGLISH);
+        assertEquals("moments ago", pretty.data("time", new Date(0)).render());
+    }
+}
--- runtime/src/main/java/io/quarkiverse/prettytime/PrettyTimeProducer.java
+++ runtime/src/main/java/io/quarkiverse/prettytime/PrettyTimeProducer.java
@@ -1,6 +1,7 @@
 package io.quarkiverse.prettytime;
 
 import javax.enterprise.inject.Produces;
+import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.ocpsoft.prettytime.PrettyTime;
@@ -9,6 +10,7 @@ import io.quarkus.arc.DefaultBean;
 
 public class PrettyTimeProducer {
 
+    @Named("prettyTime")
     @Produces
     @Singleton
     @DefaultBean
